8357e6ac0900a51946d2508b1c02eb71cdfaecdf
mingyu <mgahn0706@snu.ac.kr>
2021-11-25 04:42:14 +0900

    Prettier 적용

--- src/API/API.tsx
+++ src/API/API.tsx
@@ -1,9 +1,9 @@
-import axios, {AxiosInstance} from 'axios'
+import axios, { AxiosInstance } from "axios";
 
 //axios 를 간단하게 해주는 request.
-const request : AxiosInstance = axios.create({
-    baseURL: 'http://13.125.247.56',
-})
+const request: AxiosInstance = axios.create({
+  baseURL: "http://13.125.247.56",
+});
 /*
 * request
       .post<LogInInputType, AxiosResponse<AccessTokenType>>('/login', {
@@ -13,4 +13,4 @@ const request : AxiosInstance = axios.create({
 *  와 같이 사용하면 됩니다.
 * */
 
-export default request
+export default request;
