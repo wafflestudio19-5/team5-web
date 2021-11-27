8b637bb3ad4a1a5b6d0f8d571c816b386788b928
designdefined.by@gmail.com <designdefined.by@gmail.com>
2021-11-27 14:51:29 +0900

    레이아웃 / 로그인 테스트 전환 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import TestComponent from "./component/TestComponent/TestComponent";
 import "./App.css";
 import "./scss/main.scss";
 import Home from "./component/Home/Home";
+import { useState } from "react";
 function App() {
+  const [isLayoutDisplayed, setIsLayoutDisplayed] = useState<boolean>(false);
   return (
     <div className="App">
-      <Home />
+      {isLayoutDisplayed ? <Home /> : <TestComponent />}
+      <button
+        className="ChangeTestButton"
+        onClick={() => setIsLayoutDisplayed(!isLayoutDisplayed)}
+      >
+        {isLayoutDisplayed ? "로그인 테스트로" : "레이아웃 테스트로"}
+      </button>
     </div>
   );
 }
