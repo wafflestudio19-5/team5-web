679d66506d4d44434b3178ef978ce3fcdfb88bef
Chang-Man <changagim@snu.ac.kr>
2021-12-31 16:20:06 +0900

    left_menu UI완성

--- src/component/MainPage/Home/TotalView/TotalView.tsx
+++ src/component/MainPage/Home/TotalView/TotalView.tsx
@@ -1,10 +1,10 @@
-import TotalViewItem from "./TotalViewItem";
-import { useEffect, useState } from "react";
-import { boardDummy } from "../../../../dummy/get-dummy";
-import RightMenu from "../RightMenu/RightMenu";
-import { logout } from "../../../../redux/authorization";
-import { useDispatch } from "react-redux";
-import { deleteToken } from "../../../../function/localStorage";
+import TotalViewItem from './TotalViewItem';
+import { useEffect, useState } from 'react';
+import { boardDummy } from '../../../../dummy/get-dummy';
+import RightMenu from '../RightMenu/RightMenu';
+import { logout } from '../../../../redux/authorization';
+import { useDispatch } from 'react-redux';
+import { deleteToken } from '../../../../function/localStorage';
 
 interface boardDummyItem {
   id: string;
@@ -25,24 +25,28 @@ const TotalView = () => {
 
   return (
     <div className="TotalView">
-      <div id={"container"}>
-        <div className={"banner"}></div>
-        <div className={"leftSide"}>
+      <div id={'container'}>
+        <div className={'banner'}></div>
+        <div className={'leftSide'}>
           <div
-            className={"userCard"}
+            className={'userCard'}
             onClick={() => {
               tryLogout();
             }}
           >
             로그아웃
           </div>
-          <div className={"menu"}>메뉴</div>
-          <div className={"ad"}>광고</div>
-          <div className={"ad"}>광고</div>
-          <div className={"ad"}>광고</div>
+          <div className={'left_menu'}>
+            <li id={'my_post'}>내가 쓴 글</li>
+            <li id={'my_comment'}>댓글 단 글</li>
+            <li id={'my_clip'}>내 스크랩</li>
+          </div>
+          <div className={'ad'}>광고</div>
+          <div className={'ad'}>광고</div>
+          <div className={'ad'}>광고</div>
         </div>
-        <div className={"rightSide"}>
-          <div className={"searchBar"}>검색</div>
+        <div className={'rightSide'}>
+          <div className={'searchBar'}>검색</div>
           <RightMenu />
         </div>
         <div className="TotalView__main">
