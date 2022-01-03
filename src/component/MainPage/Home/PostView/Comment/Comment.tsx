0039bdcc99e355a33bf4d9b3c9f27ca15f9f327f
mingyu <mgahn0706@snu.ac.kr>
2022-01-04 06:11:36 +0900

    댓글 입력창 (스타일 미적용)

--- src/component/MainPage/Home/PostView/Comment/Comment.tsx
+++ src/component/MainPage/Home/PostView/Comment/Comment.tsx
@@ -15,7 +15,9 @@ const Comment = () => {
     setCommentList(commentDummy);
   });
 
-  const writeComment = () => {};
+  const [isAnonymous, setAnonymous] = useState<boolean>(false); //익명 여부
+
+  const writeComment = () => {}; // 댓글 작성 함수; API 완성시 작성 예정
 
   return (
     <div className={"Comment"}>
@@ -33,6 +35,18 @@ const Comment = () => {
           </li>
         ))}
       </ul>
+
+      <form className={"Write"}>
+        <ul className={"option"}>
+          <textarea
+            className={"content"}
+            name={"content"}
+            placeholder={"댓글을 입력하세요."}
+          />
+          <li title={"완료"} className={"submit"} />
+          <li title={"익명"} className={"anonymus"} />
+        </ul>
+      </form>
     </div>
   );
 };
