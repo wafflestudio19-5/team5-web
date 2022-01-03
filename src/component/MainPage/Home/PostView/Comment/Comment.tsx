39841b051f6f69f1a38b575fc7092d9fc35114cc
mingyu <mgahn0706@snu.ac.kr>
2022-01-04 05:51:53 +0900

    대댓글 버튼

--- src/component/MainPage/Home/PostView/Comment/Comment.tsx
+++ src/component/MainPage/Home/PostView/Comment/Comment.tsx
@@ -15,19 +15,18 @@ const Comment = () => {
     setCommentList(commentDummy);
   });
 
+  const writeComment = () => {};
+
   return (
     <div className={"Comment"}>
       <ul className={"BoardView__list"}>
         {commentList.map((item) => (
           <li key={item.id} className={"BoardView__item"}>
             <div className={"wrapper"}>
-              <h2 className={"medium_bold"}>{item.writer}</h2>
-              <ul className={"Comment__option"}>
-                <p className={"small"}> 대댓글</p>
-                <p className={"small"}> 공감</p>
-                <p className={"small"}> 쪽지</p>
-                <p className={"small"}> 신고</p>
-              </ul>
+              <div className={"Comment__header"}>
+                <h2 className={"medium_bold"}>{item.writer}</h2>
+                <p className={"small"}>대댓글</p>
+              </div>
               <p className={"medium"}>{item.content}</p>
               <p className={"small"}>{item.time}</p>
             </div>
