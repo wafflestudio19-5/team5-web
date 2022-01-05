28ea8a60fb68aca236f74bac3bc8c87a7b299df5
mingyu <mgahn0706@snu.ac.kr>
2022-01-06 07:53:55 +0900

    게시글 삭제 기능

--- src/component/MainPage/Home/PostView/PostView.tsx
+++ src/component/MainPage/Home/PostView/PostView.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { getBoardDetailDummy } from "../../../../dummy/get-dummy";
 import Comment from "./Comment/Comment";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import request from "../../../../API/API";
 
 interface PostViewParams {
@@ -17,6 +17,8 @@ interface BoardDetailDummyItem {
 }
 
 const PostView = () => {
+  const history = useHistory();
+
   const getPostDetail = () => {
     request
       .get(`/post/${path.postId}`)
@@ -36,6 +38,23 @@ const PostView = () => {
     content: "",
   });
 
+  const goBack = () => {
+    history.push(`/${path.boardId}`);
+  };
+
+  const deletePost = () => {
+    if (window.confirm("이 글을 삭제하시겠습니까?")) {
+      request
+        .delete(`/post/${path.postId}`)
+        .then(() => {
+          goBack();
+        })
+        .catch(() => {
+          console.log("게시글 삭제 실패!"); //테스트용
+        });
+    }
+  };
+
   useEffect(() => {
     getPostDetail();
   }, [setPostDetail, path.boardId, path.postId]);
@@ -49,8 +68,8 @@ const PostView = () => {
           <time>시간</time>
         </div>
         <ul>
-          <li>쪽지</li>
-          <li>신고</li>
+          <li>수정</li>
+          <li onClick={deletePost}>삭제</li>
         </ul>
       </div>
       <h2 className={"large"}>{postDetail.title}</h2>
@@ -66,6 +85,7 @@ const PostView = () => {
         <span className={"scrap"}>스크랩</span>
       </div>
       <Comment />
+      <button onClick={goBack}>글 목록</button>
     </div>
   );
 };
