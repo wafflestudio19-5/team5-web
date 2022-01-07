582d0047232bb65b32cc3ca16e874469dc356b7a
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-01-08 08:05:09 +0900

    resolve 다시

--- src/API/errorHandling.ts
+++ src/API/errorHandling.ts
@@ -10,17 +10,3 @@ export const getErrorData = (inputError: unknown) => {
     return null;
   }
 };
-
-export const consoleLogAllError = (errorData: object | null) => {
-  if (errorData) {
-    for (const [key, value] of Object.entries(errorData)) {
-      if (key === "non_field_errors") {
-        console.log(`${value}`);
-      } else {
-        console.log(`${key}: ${value}`);
-      }
-    }
-  } else {
-    console.log("에러 없이 성공!");
-  }
-};
