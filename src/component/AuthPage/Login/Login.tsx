f86ab2e713e97ac7c8b9c48ebeeb90caed5092d7
Chang-Man <changagim@snu.ac.kr>
2022-01-07 21:20:49 +0900

    login ui and login input form으로 변경

--- src/component/AuthPage/Login/Login.tsx
+++ src/component/AuthPage/Login/Login.tsx
@@ -1,18 +1,20 @@
-import { useState } from "react";
-import { login } from "../../../redux/authorization";
-import { useHistory } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { saveToken } from "../../../function/localStorage";
-import { LoginInputType } from "../../../interface/interface";
-import { postLoginAPI } from "../../../API/loginAPI";
+import { useState } from 'react';
+import { login } from '../../../redux/authorization';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { saveToken } from '../../../function/localStorage';
+import { LoginInputType } from '../../../interface/interface';
+import { postLoginAPI } from '../../../API/loginAPI';
+import { Link } from 'react-router-dom';
+import { log } from 'util';
 
 const Login = () => {
   const history = useHistory();
   const dispatch = useDispatch();
 
   const [loginInput, setLoginInput] = useState<LoginInputType>({
-    username: "",
-    password: "",
+    username: '',
+    password: '',
   });
 
   const changeUsername = (input: string) => {
@@ -21,37 +23,58 @@ const Login = () => {
   const changePassword = (input: string) => {
     setLoginInput({ ...loginInput, password: input });
   };
-  const tryLogin = (input: LoginInputType) => {
-    postLoginAPI(loginInput).then((token) => {
+  const tryLogin = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const input = {
+      username: loginInput.username,
+      password: loginInput.password,
+    };
+
+    postLoginAPI(input).then((token) => {
       dispatch(login(token));
       saveToken(token);
-      history.push("/");
+      history.push('/');
     });
   };
 
   return (
     <div className="Login">
-      <input
-        type="text"
-        name="아이디"
-        placeholder="아이디"
-        onChange={(e) => changeUsername(e.target.value)}
-        value={loginInput.username}
-      />
-      <input
-        type="password"
-        name="비밀번호"
-        placeholder="비밀번호"
-        onChange={(e) => changePassword(e.target.value)}
-        value={loginInput.password}
-      />
-      <button
-        onClick={() => {
-          tryLogin(loginInput);
-        }}
-      >
-        로그인
-      </button>
+      <div className="LoginBox">
+        <h1 className="LoginLogo" />
+        <form onSubmit={tryLogin}>
+          {' '}
+          <input
+            type="text"
+            name="아이디"
+            placeholder="아이디"
+            onChange={(e) => changeUsername(e.target.value)}
+            value={loginInput.username}
+          />
+          <input
+            type="password"
+            name="비밀번호"
+            placeholder="비밀번호"
+            onChange={(e) => changePassword(e.target.value)}
+            value={loginInput.password}
+          />
+          <button onClick={tryLogin}>로그인</button>
+        </form>
+
+        <div className="LoginCheck">
+          <label className="LoginAuto">
+            <input type={'checkbox'} />
+            로그인 유지
+          </label>
+          <p className="forgot">아이디/비밀번호 찾기</p>
+        </div>
+        <div className="register">
+          <span>에브리타임에 처음이신가요?</span>
+          <Link to={'/register'}>
+            <span>회원가입</span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
