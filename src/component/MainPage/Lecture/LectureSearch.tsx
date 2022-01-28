e859db2af59d9828e06d9bead74c59f4b28fc9e5
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-01-29 05:48:01 +0900

    강의평 서치 디버그

--- src/component/MainPage/Lecture/LectureSearch.tsx
+++ src/component/MainPage/Lecture/LectureSearch.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { EvalType, SearchedLectureType } from "../../../interface/interface";
 import { getRecentLecture, postSearchLecture } from "../../../API/lectureAPI";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 interface LectureSearchParams {
   searchValue: string;
 }
 const LectureSearch = () => {
   const params = useParams<LectureSearchParams>();
+  const history = useHistory();
   const [searchedLecture, setSearchedLecture] = useState<SearchedLectureType[]>(
     []
   );
@@ -23,12 +24,16 @@ const LectureSearch = () => {
 
   return (
     <ul className="Lecture__list">
-      <li key="label" className="Lecture__item-label search"></li>
-
       {searchedLecture.length > 0 ? (
         searchedLecture.map((lecture) => {
           return (
-            <li key={lecture.id} className="Lecture__item">
+            <li
+              key={lecture.id}
+              className="Lecture__item"
+              onClick={() => {
+                history.push(`/lecture/view/${lecture.id}`);
+              }}
+            >
               <p className="title">
                 {lecture.title} <span>{lecture.instructor}</span>
               </p>
